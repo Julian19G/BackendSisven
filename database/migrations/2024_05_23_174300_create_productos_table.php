59dd3b6fdb071b42a6cec0f5cbd29a72610367c5
Julian19G <Julian19guerra@gmail.com>
2024-05-23 13:15:45 -0500

    Ultimo cambio

--- database/migrations/2024_05_23_174300_create_productos_table.php
+++ database/migrations/2024_05_23_174300_create_productos_table.php
@@ -1,31 +1,35 @@
-<?php
+    <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
-use Illuminate\Support\Facades\Schema;
+    use Illuminate\Database\Migrations\Migration;
+    use Illuminate\Database\Schema\Blueprint;
+    use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    return new class extends Migration
     {
-        Schema::create('productos', function (Blueprint $table) {
-            $table->id();
-            $table->string('nombre', 80)->nullable();
-            $table->integer('precio')->nullable();
-            $table->integer('cantidad')->nullable();
-            $table->timestamps();
-            $table->foreign('id_categoria')->references('id')->on('categorias')->onDelete('set null');
-        });
-    }
+        /**
+         * Run the migrations.
+         */
+        public function up(): void
+        {
+            Schema::create('productos', function (Blueprint $table) {
+                $table->id();
+                $table->string('nombre', 80)->nullable();
+                $table->integer('precio')->nullable();
+                $table->integer('cantidad')->nullable();
+                $table->unsignedBigInteger('id_categoria')->nullable(); // Add the column explicitly
+                $table->timestamps();
+    
+                $table->foreign('id_categoria')->references('id')->on('categorias')->onDelete('set null');
+            });
+        }
+    
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
-    {
-        Schema::dropIfExists('productos');
-    }
-};
+
+        /**
+         * Reverse the migrations.
+         */
+        public function down(): void
+        {
+            Schema::dropIfExists('productos');
+        }
+    };
